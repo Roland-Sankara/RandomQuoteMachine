cb231ed115bfb839226549e382f32f7dfb18a615
Roland-Sankara <roland1sankara@gmail.com>
2021-05-12 15:46:55 +0300

    add componentsfolder

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div>
+            <h1>Random Quote Machine</h1>
+            <h3>Loading...</h3>
+        </div>
+    )
+}
+
+export default App;
